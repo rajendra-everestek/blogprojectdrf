b291918af49f445d3d621c64d9185e3f9d30734f
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 02:24:29 +0530

    WC5

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -4,9 +4,9 @@
 # sudo yum install -y nginx
 # sudo yum install -y virtualenv
 
-sudo yum install -y python3-pip
+sudo yum install -y python2-pip
 sudo amazon-linux-extras install nginx1 #centos 7
-sudo python3 -m venv virtualenv
+sudo python2 -m venv virtualenv
 
 # sudo amazon-linux-extras install epel
 # sudo yum install python-devel python-setuptools python-pip
