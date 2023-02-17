624676881268f1f2fe1b7c86d9409ba31fc663e1
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-17 20:34:03 +0530

    WC 5

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# sudo apt install -y nginx #ubantu
+# sudo yum install -y virtualenv
+
 sudo yum install -y python3-pip
-# sudo yum install -y nginx #ubantu
 sudo amazon-linux-extras install nginx1 #centos 7
-sudo yum install -y virtualenv
+python3 -m venv python3-virtualenv
