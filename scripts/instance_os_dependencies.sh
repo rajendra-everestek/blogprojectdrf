e393f56f169a1b6dae5ef3b55f9c6689e903c121
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-17 20:11:13 +0530

    Working code 3

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install -y python3-pip
-sudo apt install -y nginx
-sudo apt install -y virtualenv
+sudo yum install -y python3-pip
+sudo yum install -y nginx
+sudo yum install -y virtualenv
