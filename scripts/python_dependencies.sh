427554d732c9406561d22faded8285f62396433e
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-17 21:20:31 +0530

    WC5

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,4 +2,5 @@
 
 sudo python3 -m venv /home/ec2-user/env
 source /home/ec2-user/env/bin/activate
+/home/ec2-user/env/bin/python3 -m pip install --upgrade pip
 pip install -r /home/ec2-user/blogprojectdrf/requirements.txt
