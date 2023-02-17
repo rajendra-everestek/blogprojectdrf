0f04858567495ae82035cf1b5f1268593f545c3d
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 01:39:48 +0530

    WC5

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-# sudo apt install -y nginx #ubantu
-# sudo yum install -y virtualenv
+# sudo apt install -y python3-pip
+# sudo apt install -y nginx
+# sudo apt install -y virtualenv
 
-# sudo yum install -y python3-pip
+sudo yum install -y python3-pip
 sudo amazon-linux-extras install nginx1 #centos 7
-# sudo python3 -m venv virtualenv
+sudo python3 -m venv virtualenv
+
+sudo amazon-linux-extras install epel
+# sudo yum install python-devel python-setuptools python-pip
+sudo pip install --upgrade pip
+sudo pip install virtualenv
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# sudo python3 -m venv /home/ec2-user/env
-sudo amazon-linux-extras install epel
-sudo yum install python-devel python-setuptools python-pip
-sudo pip install --upgrade pip
-sudo pip install virtualenv
+# virtualenv /home/ubuntu/env
+# source /home/ubuntu/env/bin/activate
+# pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
+
+
+
 virtualenv /home/ec2-user/env
 source /home/ec2-user/env/bin/activate
 /home/ec2-user/env/bin/python -m pip install --upgrade pip
