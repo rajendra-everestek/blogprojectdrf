8644275d2d0ae2840b4d2e317634c00c62eb9b45
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 00:20:49 +0530

    WC5

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-sudo python3 -m venv /home/ec2-user/env
+# sudo python3 -m venv /home/ec2-user/env
+sudo amazon-linux-extras install epel
+sudo yum install python-devel python-setuptools python-pip
+sudo pip install --upgrade pip
+sudo pip install virtualenv
+virtualenv /home/ec2-user/env
 source /home/ec2-user/env/bin/activate
-sudo /home/ec2-user/env/bin/python3 -m pip install --upgrade pip
 pip install -r /home/ec2-user/blogprojectdrf/requirements.txt
