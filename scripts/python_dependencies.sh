a0cfc3da982bf672fc7ba69ebd0c84f6830f80f3
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-18 02:02:16 +0530

    W

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-virtualenv /home/ubuntu/env
-source /home/ubuntu/env/bin/activate
-pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
+# virtualenv /home/ubuntu/env
+# source /home/ubuntu/env/bin/activate
+# pip install -r /home/ubuntu/blogprojectdrf/requirements.txt
+
+
+
+virtualenv /home/ec2-user/env
+source /home/ec2-user/env/bin/activate
+/home/ec2-user/env/bin/python -m pip install --upgrade pip
+pip install -r /home/ec2-user/blogprojectdrf/requirements.txt
