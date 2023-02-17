65a2a861755481610df896656ae63327bdff798a
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-17 20:26:43 +0530

    Working code 4

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sudo yum install -y python3-pip
-sudo yum install -y nginx
+# sudo yum install -y nginx #ubantu
+sudo amazon-linux-extras install nginx1 #centos 7
 sudo yum install -y virtualenv
